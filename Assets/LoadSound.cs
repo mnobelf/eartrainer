bea40de0eeaf85914084aaa54d87939720d0b297
Muhammad Rizki Fonna <13516001@std.stei.itb.ac.id>
2021-03-17 14:46:16 +0700

    Add sound to training scene

---
+++ Assets/LoadSound.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LoadScene : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public void LoadTrainingScene(string scenename)
+    {
+        SceneManager.LoadScene(scenename);
+    }
+
+}
