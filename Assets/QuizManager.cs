6c97b41d11f0535c26b9098bd4339d080bb2c365
Nobelf <13517042@std.stei.itb.ac.id>
2021-03-31 04:29:08 +0700

    update interval quiz system

---
+++ Assets/QuizManager.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class QuizManager : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+    public int answer;
+    public RandomInterval generator;
+
+    void AskGenerator()
+    {
+        generator.PlaySound();
+    }
+
+    public void SetKeyAnswer(int input)
+    {
+        answer = input;
+    }
+
+    public void CheckAnswer(int input)
+    {
+        if (input == answer) {
+            Debug.Log("Correct");
+            Invoke("AskGenerator",1f);
+        } else {
+            Debug.Log("Incorrect");
+        }
+    }
+
+    void Start()
+    {
+        Invoke("AskGenerator",1f);
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
