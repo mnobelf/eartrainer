a6cc04d231bb015e0d87d5e9301bf715c6fa7906
Muhammad Rizki Fonna <13516001@std.stei.itb.ac.id>
2021-04-07 13:34:55 +0700

    Make sound input from microphone on training scene

---
+++ Assets/SoundInput.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SoundInput : MonoBehaviour
+{
+    public AudioSource audioSource;
+    public int audioSampleRate = 44100;
+    public string microphone;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void BtnClick()
+    {
+        Debug.Log("terpencet");
+        foreach (string device in Microphone.devices)
+        {
+            if (microphone == null)
+            {
+                microphone = device;
+            }
+            //options.Add(device);
+        }
+        audioSource.Stop();
+        audioSource.clip = Microphone.Start(microphone, false, 10, audioSampleRate);
+        audioSource.loop = false;
+        Debug.Log(Microphone.IsRecording(microphone).ToString());
+
+        if (Microphone.IsRecording(microphone))
+        { 
+            while (!(Microphone.GetPosition(microphone) > 0))
+            {
+            } 
+
+            Debug.Log("recording started with " + microphone);
+
+        }
+        else
+        {
+
+            Debug.Log(microphone + " doesn't work!");
+        }
+
+    }
+}
