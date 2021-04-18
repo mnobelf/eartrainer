ae05038176c71686ff00a105ecf65b8a50452a84
Nobelf <13517042@std.stei.itb.ac.id>
2021-04-19 02:20:27 +0700

    make pitch analyzer module

--- Assets/GenerateInterval.cs
+++ Assets/GenerateInterval.cs
@@ -5,22 +5,8 @@ using UnityEngine.UI;
 
 public class GenerateInterval : MonoBehaviour
 {
-    public float RmsValue;
-    public float DbValue;
-    public float PitchValue;
-    public float Pitch;
+
     public float pi_mic;
-    
-
-    private const int QSamples = 1024;
-    private const float RefValue = 0.1f;
-    private const float Threshold = 0.02f;
-
-    float[] _samples;
-    private float[] _spectrum;
-    private float _fSample;
-
-
 
     int index_interval;
     int index_not;
@@ -32,15 +18,11 @@ public class GenerateInterval : MonoBehaviour
     public QuizManager QuizManager;
     public Text txt;
     public AudioSource[] aus;
+    public PitchAnalyzer analyzer;
     
-
     // Start is called before the first frame update
     void Start()
     {
-        _samples = new float[QSamples];
-        _spectrum = new float[QSamples];
-        _fSample = AudioSettings.outputSampleRate;
-
 
         index_interval = Random.Range(0, 12);
         interval_name = new string[12];
@@ -97,61 +79,16 @@ public class GenerateInterval : MonoBehaviour
         pitchAnswer = pitchNot[aus[index_not].name] + (index_interval + 1);
         Debug.Log(pitchAnswer.ToString());
 
-
-
-
-
-
     }
 
     // Update is called once per frame
     void Update()
     {
-        pi_mic = AnalyzeSound(aus[25]);
+        pi_mic = analyzer.AnalyzeSound(aus[25]);
         if ((pi_mic>=(pitchAnswer-1)) && ((pi_mic <= (pitchAnswer + 1))))
         {
             txt.text = "Answer Correct";
         }
     }
 
-    float AnalyzeSound(AudioSource x)
-    {
-        x.GetOutputData(_samples, 0); // fill array with samples
-        int i;
-        float sum = 0;
-        for (i = 0; i < QSamples; i++)
-        {
-            sum += _samples[i] * _samples[i]; // sum squared samples
-        }
-        RmsValue = Mathf.Sqrt(sum / QSamples); // rms = square root of average
-        DbValue = 20 * Mathf.Log10(RmsValue / RefValue); // calculate dB
-        if (DbValue < -160) DbValue = -160; // clamp it to -160dB min
-                                            // get sound spectrum
-        x.GetSpectrumData(_spectrum, 0, FFTWindow.BlackmanHarris);
-        float maxV = 0;
-        var maxN = 0;
-        for (i = 0; i < QSamples; i++)
-        { // find max 
-            if (!(_spectrum[i] > maxV) || !(_spectrum[i] > Threshold))
-                continue;
-
-            maxV = _spectrum[i];
-            maxN = i; // maxN is the index of max
-        }
-        float freqN = maxN; // pass the index to a float variable
-        if (maxN > 0 && maxN < QSamples - 1)
-        { // interpolate index using neighbours
-            var dL = _spectrum[maxN - 1] / _spectrum[maxN];
-            var dR = _spectrum[maxN + 1] / _spectrum[maxN];
-            freqN += 0.5f * (dR * dR - dL * dL);
-        }
-        PitchValue = freqN * (_fSample / 2) / QSamples; // convert index to frequency
-        Pitch = FrequencytoPitchConverter(PitchValue);
-        return Pitch;
-    }
-
-    float FrequencytoPitchConverter(float freq)
-    {
-        return (12 * Mathf.Log(freq / 440, 2) + 49);
-    }
 }
